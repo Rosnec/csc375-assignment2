ed3fe5cc990e26ae3570ab41720b757140de1581
Rosnec <rosnec.dev@gmail.com>
2013-11-15 10:06:04 -0500

    aruuughhhfhfhf work already

--- src/rwl/locks/countdown_semaphore_lock.clj
+++ src/rwl/locks/countdown_semaphore_lock.clj
@@ -36,18 +36,18 @@
              (loop [held-latch nil]
                (.await @writer-latch)
                (let [active-latch @writer-latch]
-               (if (locking active-latch
-                     (if (= held-latch active-latch)
-                       (.tryAcquire reader-P)
-                       (if (and (< (.availablePermits reader-P) permits)
-                                (zero? @waiting-writers))
-                         (.tryAcquire reader-P))))
-               ; acquired a permit
-                 (let [deref-x @atomic-x]
-                   (.release reader-P)
-                   deref-x)
-               ; did not acquire a permit
-                 (recur active-latch)))))]
+                 (if (locking active-latch
+                       (if (= held-latch active-latch)
+                         (.tryAcquire reader-P)
+                         (if (and (< (.availablePermits reader-P) permits)
+                                  (zero? @waiting-writers))
+                           (.tryAcquire reader-P))))
+                 ; acquired a permit
+                   (let [deref-x @atomic-x]
+                     (.release reader-P)
+                     deref-x)
+                 ; did not acquire a permit
+                   (recur active-latch)))))]
        (general-rwl read-fn write-fn))))
                    
                
