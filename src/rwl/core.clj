e15c0af8973fe870ec756ec214da327ff6f5dcf6
Rosnec <rosnec.dev@gmail.com>
2013-11-15 17:21:23 -0500

    blah

--- src/rwl/core.clj
+++ src/rwl/core.clj
@@ -7,9 +7,9 @@
   "Runs throughput tests and outputs to the command-line argument -- outdir"
   ([outdir]
      (throughput-tests outdir 2 9 0.01))
-     (-main outdir 2 9))
   ([outdir start]
      (throughput-tests outdir start 9 0.01))
   ([outdir start iterations]
-     (throughput-tests outdir start iterations 0.01))
+     (throughput-tests outdir start iterations 0.01)))
+
 
