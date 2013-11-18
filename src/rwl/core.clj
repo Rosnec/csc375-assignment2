274652252c522e96f474302c9caace445e049eb5
Rosnec <rosnec.dev@gmail.com>
2013-11-18 00:40:12 -0500

    come oooonnnn

--- src/rwl/core.clj
+++ src/rwl/core.clj
@@ -1,13 +1,20 @@
 (ns rwl.core
   {:author "Dan Wysocki"}
   (:require [rwl.star-flight-3d :refer [throughput-tests]]
-            [rwl.locks.reentrant-rwl :refer [RRWL-aarray]])
+            [rwl.locks.reentrant-rwl :refer [RRWL-aarray]]
+            [rwl.util :refer [dopool]])
+  (:import [util.java IntArray])
   (:gen-class))
 
 (defn -main
   "Runs throughput tests and outputs to the command-line argument -- outdir"
   ([]
-     (println (RRWL-aarray Integer 10)))
+     (let [arr (new IntArray 1000)
+           data (range 1000)]
+       (dopool #(.append arr %) data 100)
+       (println "data" (apply + data)
+                "arr" (apply + (for [idx data]
+                                 (.get arr idx))))))
   ([outdir]
      (throughput-tests outdir 2 9 0.01))
   ([outdir start]
